21837473adbbba0c4f1ff0eb84d09319b00effd8
gdany91 <you@example.comdgarofalo52@gmail.com>
2023-02-07 10:26:25 +0100

    crud

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,7 +4,60 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Saint;
+
 class MainController extends Controller
 {
-    //
+    // --- INDEX
+    public function home() {
+
+        $saints = Saint::orderBy('created_at', 'DESC') -> get();
+
+  
+        return view('pages.home', compact('saints'));
+    }
+
+    // --- SHOW
+    public function saintShow($id) {
+
+        $saint = Saint::find($id);
+
+        $data = [
+            'saint' => $saint
+        ];
+
+        return view('pages.saintShow', $data);
+    }
+
+    // --- DELETE
+    public function saintDestroy($id) {
+
+        $saint = Saint::find($id);
+        $saint -> delete();
+
+        return redirect() -> route('home');
+    }
+
+    // --- CREATE
+    public function saintCreate() {
+
+        return view('pages.saintCreate');
+    }
+
+    public function saintStore(Request $request) {
+
+        $data = $request->all();
+
+
+        $saint = new Saint();
+
+        $saint -> name = $data['name'];
+        $saint -> birdPlace = $data['birdPlace'];
+        $saint -> blessingDate = $data['blessingDate'];
+        $saint -> miracleCount = $data['miracleCount'];
+
+        $saint -> save();
+        
+        return redirect() -> route('home');
+    }
 }
